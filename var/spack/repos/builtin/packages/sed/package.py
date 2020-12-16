4b9a37ee2fbffc02b98f11a1efe7a7323937a7d2
Andre Sailer <andre.philippe.sailer@cern.ch>
2020-12-16 17:15:59 +0100

    sed: different compression for later tarballs

--- var/spack/repos/builtin/packages/sed/package.py
+++ var/spack/repos/builtin/packages/sed/package.py
@@ -4,11 +4,17 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
-
+from spack.version import Version
 
 class Sed(AutotoolsPackage, GNUMirrorPackage):
     """GNU implementation of the famous stream editor."""
     homepage = "http://www.gnu.org/software/sed/"
-    gnu_mirror_path = "sed/sed-4.2.2.tar.bz2"
+    gnu_mirror_path = None
 
     version('4.2.2', sha256='f048d1838da284c8bc9753e4506b85a1e0cc1ea8999d36f6995bcb9460cddbd7')
+
+    @property
+    def gnu_mirror_path(self):
+        if self.spec.version > Version('4.2.2'):
+            return "sed/sed-4.3.tar.xz"
+        return "sed/sed-4.2.2.tar.bz2"
